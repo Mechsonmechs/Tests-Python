6636918817a2b6a5068fa2aff858d251c5350134
Mechsonmechs <samt2992@googlemail.com>
2018-12-18 14:10:27 +0000

    Adding first Python file

---
+++ firstFile.py
@@ -0,0 +1,9 @@
+scoobly = {
+    'Leader': 'Sam',
+    'Vice': 'Luke',
+    'Member01': 'James'
+}
+if scoobly['Leader'] = 'Sam':
+    print("You're in for a winner.")
+else:
+    print("That's a shame.")
