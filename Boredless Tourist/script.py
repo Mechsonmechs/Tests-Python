221f0a14ee5134e5ff96b82e1dd10058051919cf
Mechsonmechs <samt2992@googlemail.com>
2018-12-18 14:21:24 +0000

    Adding first basic data

--- Boredless Tourist/script.py
+++ Boredless Tourist/script.py
@@ -1,1 +1,3 @@
-scoobly = []
+destinations = [['Paris', 'France'], ['Shanghai', 'China'], ['Los Angeles', 'USA'], ['Sao Paulo', 'Brazil'], ['Cairo', 'Egypt']]
+
+test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
