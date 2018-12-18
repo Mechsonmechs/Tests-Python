a2b80b38e8b15b269c86afb2b0438d7ae6301d69
Mechsonmechs <samt2992@googlemail.com>
2018-12-18 14:54:41 +0000

    Changes that don't appear to run but lookg good.

--- Boredless Tourist/script.py
+++ Boredless Tourist/script.py
@@ -1,3 +1,20 @@
-destinations = [['Paris', 'France'], ['Shanghai', 'China'], ['Los Angeles', 'USA'], ['Sao Paulo', 'Brazil'], ['Cairo', 'Egypt']]
+destinations = ["Paris, France","Shanghai, China","Los Angeles, USA", "São Paulo, Brazil","Cairo, Egypt"]
 
 test_traveller = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+def get_destination_index(destination):
+	destination_index = destinations.index(destination)
+  	return destination_index
+
+## print(get_destination_index("Los Angeles, USA"))
+
+def get_traveller_index(traveler):
+	traveler_destination = traveler[1]
+	traveler_destination_index = get_destination_index(traveler_destination)
+	return traveler_destination_index
+
+print(get_traveller_index(test_traveller))
+
+
+## Wait for input allow read  ##
+raw_input()
